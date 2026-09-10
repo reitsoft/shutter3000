45da0625132249c129be842fb31f0081338b6646
reitsoft <79015121+reitsoft@users.noreply.github.com>
2026-09-10 12:51:33 +0000

    PWA Typ referenzen ergänzt

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,5 +1,9 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
+
+/// <reference types="vite-plugin-pwa/client" />
+/// <reference types="vite-plugin-pwa/info" />
+
 declare global {
 	namespace App {
 		// interface Error {}
